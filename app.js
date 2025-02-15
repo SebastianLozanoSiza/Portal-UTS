5ccb7c3406f9aaf2697df67e59d881bea6c69b07
SebastianLozanoSiza <jsebastianlozano@uts.edu.co>
2025-02-15 10:58:39 -0500

    agregada la funcionalidad de dinamismo para las cartas

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log(students);
+
+/* 1. Llenar dinamicamente select de nombres */
+students.forEach((student) => {
+    const option = document.createElement('option');
+    option.value = student.nombre;
+    option.textContent = student.nombre;
+    document.querySelector('#nombre').appendChild(option);
+})
+
+/* 2. Llenar dinamicamente select con sueldos en dolares */
+const max = 100;
+const min = max - 90;
+
+for (let i = min; i <= max; i++) {
+    const opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.textContent = i;
+    document.querySelector('#sueldo').appendChild(opcion);
+}
+
+/* 3. Event listeners */
+document.addEventListener('DOMContentLoaded', () => {
+    showStudents(students);
+})
+
+function showStudents(students) {
+    const cardContainer = document.querySelector('#tarjetas');
+    students.forEach((student) => {
+        const studentHtml = document.createElement('p');
+        studentHtml.innerHTML = `
+            <figure class="student">
+                <img src="img/${student.imagen}" alt="Student" />
+                    <div class="student-box">
+                        <h3>${student.nombre}</h3>
+                        <ul class="student-details">
+                            <li>
+                                <span>${student.experto}</span>
+                            </li>
+                            <br />
+                            <li>
+                                <strong>${student.ingles}</strong>
+                            </li>
+                        </ul>
+                        <div class="student-price">
+                            <strong>$${student.sueldo}/hora</strong>
+                            <a href="#" class="btn btn--small">Contratar</a>
+                        </div>
+                    </div>
+            </figure>
+        `;
+        cardContainer.appendChild(studentHtml);
+    })
+}
---
+++ db.js
@@ -0,0 +1,34 @@
+const students = [
+    {
+        id: 1,
+        nombre: "Alex Pena",
+        imagen: "Alex Pena.jpeg",
+        experto: "Java",
+        ingles: "Beginner",
+        sueldo: 50
+    },
+    {
+        id: 2,
+        nombre: "Felipe Garcia",
+        imagen: "Felipe Garcia.jpeg",
+        experto: "JavaScript",
+        ingles: "Advanced",
+        sueldo: 60
+    },
+    {
+        id: 3,
+        nombre: "Susanita",
+        imagen: "Susanita.jpeg",
+        experto: "NodeJS",
+        ingles: "Middle",
+        sueldo: 80
+    },
+    {
+        id: 4,
+        nombre: "Marlon",
+        imagen: "Marlon.jpeg",
+        experto: "Android",
+        ingles: "Middle",
+        sueldo: 100
+    }
+]
